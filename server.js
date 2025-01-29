e4b4b1892a8ee95268c44a989b5e5db6cbe4f96a
alexanderdombroski <dom21013@byui.edu>
2025-01-29 13:28:56 -0700

    finished routing assignment

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from 'express';
+import url from 'url';
+import path from 'path';
 
 const app = express();
 
-const name = process.env.NAME;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Use middleware to set up Public resource folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route the Pages
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/page1.html'))
+});
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/page2.html'))
 });
 
-const PORT = 3000;
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
